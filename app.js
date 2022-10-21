913740110be0a6f9004d82e6ee923ca76985cde1
zhw <770750592@qq.com>
2022-10-21 10:46:52 +0800

    first

---
+++ app.js
@@ -0,0 +1,19 @@
+
+const Koa = require('koa')
+const app = new Koa()
+// 绑定第一层中间件
+const respDurationMiddleware = require('./middleware/koa_response_duration')
+app.use(respDurationMiddleware)
+// 绑定第二层中间件
+const respHeaderMiddleware = require('./middleware/koa_response_header')
+app.use(respHeaderMiddleware)
+// 绑定第三层中间件
+const respDataMiddleware = require('./middleware/koa_response_data')
+app.use(respDataMiddleware)
+
+app.listen(3000)
+
+const webSocketService = require('./service/web_socket_service')
+// 开启服务端的监听, 监听客户端的连接
+// 当某一个客户端连接成功之后, 就会对这个客户端进行message事件的监听
+webSocketService.listen()
